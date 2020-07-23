ad76a4d62d2be952267a28671ecc2621df4e04d1
CC <cici.j.zhang@gmail.com>
2020-07-23 17:26:25 -0400

    got my history box running

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,17 +1,20 @@
 const teacup = {}
 
+teacup.clearInput = () => $('#userInput').val("")
+// this function clears the user input
+
+
+
 teacup.listener = function () {
    
     $('#submitButton').on('click', function (event) {
         event.preventDefault()
-        // console.log('click works!')  CC GET RID OF THIS
         const userInput = $('#userInput').val()
-        console.log(userInput, 'userInput works')
-
-    $('.result').html(`<p>${userInput}</p> <p>YOU CAN DO IT!</p>`)
-
-    // next time work on randomizer and also regex for text input 
-    // error handling 
+        // console.log(userInput, 'userInput works') CC GET RID OF THIS 
+        $('.result').append(`<p>${userInput}</p> <p>YOU CAN DO IT!</p>`)
+        teacup.clearInput()
+       console.log(typeof(userInput)) 
+       // user input is a string 
 
     })
 
