e1f2110ef242eeb20d269c7563fa78bd1c582bae
CC <cici.j.zhang@gmail.com>
2020-07-23 21:13:08 -0400

    i don't know what i did but it works

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,27 +1,51 @@
 const teacup = {}
 
+teacup.phrases = [
+    "You got this!",
+    // `YES YOU ${word}`,
+    // `I'm sure you ${word}, believe in yourself!`,
+    "Believe in the me who believes in you.",
+    // `Yes you ${word} but please remember to take care of yourself!`,
+    "KEEP GOING!!!"
+]
+
+// console.log(teacup.phrases[0])
+
+// Get words splits the userInput strings into an array
+
+// console.log(teacup.phrases.length)
+
+// clearInput() method clears the user input
 teacup.clearInput = () => $('#userInput').val("")
-// this function clears the user input
 
+teacup.stringToWords = myStr => myStr.split(" ")
 
-
+// listener() method waits for a click then it adds the user's input to the history box before clearing the input field.
 teacup.listener = function () {
-   
     $('#submitButton').on('click', function (event) {
         event.preventDefault()
-        const userInput = $('#userInput').val()
-        // console.log(userInput, 'userInput works') CC GET RID OF THIS 
-        $('.result').append(`<p>${userInput}</p> <p>YOU CAN DO IT!</p>`)
+        teacup.userInput = $('#userInput').val()
+        const arr = teacup.stringToWords(teacup.userInput)
+        console.log(arr)
+        $('.result').append(`<p>${teacup.userInput}</p> <p>YOU CAN DO IT!</p>`)
+        
+        // stringToWords[0]
+        // console.log(stringToWords[0])
         teacup.clearInput()
-       console.log(typeof(userInput)) 
-       // user input is a string 
-
     })
-
 } 
 
 
+
+teacup.init = () => {
+    teacup.listener()
+}
+
 // document ready 
 $(function(){
-    teacup.listener()
+    console.log(teacup.phrases)
+    teacup.init()
+    teacup.userInput = $('#userInput').val()
+    console.log(teacup.userInput)
 })
+
